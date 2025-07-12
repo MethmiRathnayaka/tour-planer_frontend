1e925bcb012fe6d53724681c4dde4150788375eb
MethmiRathnayaka <155360000+MethmiRathnayaka@users.noreply.github.com>
2025-07-12 12:04:19 +0530

    capitalsimple error

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './login.css';
 import { useContext } from 'react';
 import { StoreContext } from '../../Context/store-context';
 import axios from "axios"
